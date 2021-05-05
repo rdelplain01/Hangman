3a371715d5f1fe4f5c308801589c09f8532991be
rdelplain01 <62453350+rdelplain01@users.noreply.github.com>
2021-05-05 14:28:05 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 import java.util.*;
 import java.util.ArrayList;
-//import java.sql.*;
+import java.util.Random;
 
 class Main {
   public static void printArray(ArrayList<Character> array) {
@@ -11,18 +11,40 @@ class Main {
   }
 
   public static void main(String[] args) {
-    //Connection c = null;
+    //asign words
+    String[] Easy = {"bell", "blue", "fireworks", "flag", "hat", "heart", "ice cream", "panic", "pie", "red", "rocket", "star", "statue", "stripes", "white", "wreath", "boat", "chosen", "crowed", "heal"};
+    String[] Medium = {"abash", "abate", "basin", "bemoan", "canny", "capable", "capablity", "decipher", "defunct", "efficent", "elliptical", "finance", "frugality", "gregarious", "habitat", "hereditary", "hybrid", "immerse", "immigrant", "jurisdiction", "kaleidoscope"};
+    String[] Hard = {"affix", "askew", "bagpipes", "bandwagon", "crypt", "cycle", "dirndl", "daiquiri", "embezzle", "equip", "facking", "fishhook", "gossip", "galaxy", "haiku", "haphazard", "injury", "jazziest", "jinx", "knapsack"};
+
+    //asign variables
     Scanner scan = new Scanner(System.in);
+    Random rand = new Random();
     String ans = "test word";
     ArrayList<Character> guessWord = new ArrayList<Character>(1);
     ArrayList<Character> guesedLetters = new ArrayList<Character>(1);
     char guess;
+    char level;
     int fails = 0;
+    int randomNum;
     boolean win = false;
 
-    // //acces sql
-    // Class.forName("org.sqlite.replit");
-    // c = DriverManager.getConnection("hangeman.db");
+    //start game and ask for level
+    System.out.println("Welcome to Hange Man!!");
+    System.out.println("Pick a Level: \nEasy\nMedium\nHard");
+    level = scan.next().charAt(0);
+    while(level != 'E' && level != 'e' && level != 'M' && level != 'm' && level != 'H' && level != 'h') {
+      level = scan.next().charAt(0);
+    }
+
+    //cheak level
+    randomNum = rand.nextInt(19);
+    if(level == 'E' || level == 'e') {
+      ans = Easy[randomNum];
+    } else if(level == 'M' || level == 'm') {
+      ans = Medium[randomNum];
+    } else if(level == 'H' || level == 'h') {
+      ans = Hard[randomNum];
+    }
 
     //add word sequence
     for(int i = 0; i < ans.length(); i++) {
@@ -61,7 +83,6 @@ class Main {
       for(int i = 0; i < guesedLetters.size(); i++) {
         if(guess == guesedLetters.get(i)) {
           fail = false;
-          //guessed = true;
         }
       }
 
@@ -124,7 +145,6 @@ class Main {
         System.out.println("|");
       }
 
-      //System.out.println(guessWord);
       printArray(guessWord);
       guesedLetters.add(guess);
     }
